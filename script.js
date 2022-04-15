ed6dec3740b7084043be05df71dac26c093a81cf
Peyton Bechard <pbechard@my.yorku.ca>
2022-04-15 18:39:51 +0800

    Adds password-matching check in script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+Form Validation
+by Peyton Bechard
+
+Created on 15 Apr 2022
+*/
+
+// Toggles warning labels on input fields
 const inputs = document.querySelectorAll('input');
 inputs.forEach( (input) => {
     input.addEventListener('focusout', () => {
@@ -10,5 +18,26 @@ inputs.forEach( (input) => {
     });
 });
 
+
+// Confirms password matching
 const password = document.getElementById('pwd');
 const confirmPassword = document.getElementById('confirm-pwd');
+
+password.addEventListener('focusout', () => {
+    if (confirmPassword.matches(':not(place-holder-shown)') && confirmPassword.value){
+        if (confirmPassword.value === password.value) {
+            confirmPassword.style.backgroundColor = 'rgba(54, 172, 84, 0.2)';
+        } else {
+            confirmPassword.style.backgroundColor = 'rgba(172, 54, 54, 0.2)';
+        }
+    }
+});
+confirmPassword.addEventListener('focusout', () => {
+    if (confirmPassword.matches(':not(place-holder-shown)') && confirmPassword.value){
+        if (confirmPassword.value === password.value) {
+            confirmPassword.style.backgroundColor = 'rgba(54, 172, 84, 0.2)';
+        } else {
+            confirmPassword.style.backgroundColor = 'rgba(172, 54, 54, 0.2)';
+        }
+    }
+});
